217843a0483cd91fd6322967b2c5a5bf24be941c
nivedikachoudhary2 <nivedikachoudhary2@gmail.com>
2025-03-17 16:17:16 +0000

    Added new demo

---
+++ datatypes-2.js
@@ -0,0 +1,33 @@
+let productPrice = 150000;
+
+let productDiscount = 15;
+
+let productName = "Apple Iphone 11 Pro max (512 GB, Blue)";
+
+let isAvailable = true;
+
+let minusZero = -0;
+console.log(minusZero);
+
+let x;
+console.log(x);
+
+let y = null;
+console.log(y);
+
+let veryBigNumber1 = 2764646464738388337722666338338887266363333333333333447776366363663634554544;
+console.log(veryBigNumber1);
+// It print in scientific notation
+
+let veryBigNumber2 = 2764646464738388337722666338338887266363333333333333447776366363663634554544n;
+console.log(veryBigNumber2);
+
+//objects
+let product = {
+    name: "Apple Iphone 11 Pro max (512 GB, Blue)",
+    "price": 150000,
+    discount: 15,
+    isAvailable: true
+};
+
+console.log(product);
